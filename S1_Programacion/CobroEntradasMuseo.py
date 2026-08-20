70beee037fa8af201ef15ea46245db8a55320588
Sorkes-U-ing24 <al07322002@tecmilenio.mx>
2026-08-20 15:01:22 -0600

    Proceso de descuentos

--- S1_Programacion/CobroEntradasMuseo.py
+++ S1_Programacion/CobroEntradasMuseo.py
@@ -14,5 +14,38 @@ while True:
         print("Invalido, repetir.")
     else:   
         break   
+    # paso 2. proceso de visitantes
+for i in range(visitantes):
+    while True:     
+        edad=int(input("que edad tiene?"))    
+        if edad<0:
+            print("error, repetir")
+            continue 
+        break
+    if edad<3:
+        precio_base = 0
+        print("Es menor, no paga")
+        continue
+    elif edad <=17:
+        precio_base = PRECIO_MENOR
+        print("menor de edad, paga 30")
+    else    :
+        precio_base = PRECIO_MAYOR
+        print("mayor de edad, paga 45")
+
+    rol = input(" tienes algun rol?(estuadiante / profesor / adulto mayor/ ninguno)")
+    if precio_base >0:
+        if rol == "adulto mayor":
+            descuento = DESCUENTO_ADULTO_MAYOR
+        elif rol == "estudiante":
+            descuento = DESCUENTO_ESTUDIANTE
+        elif  rol == "profesor":
+            descuento = DESCUENTO_PROFESOR
+        else:
+            descuento = 0
+        precio_final = precio_base-(precio_base*descuento)
+        COBRO_TOTAL+=precio_final
+        print(f"va a pagar: ${precio_final}")
+        
 
         
