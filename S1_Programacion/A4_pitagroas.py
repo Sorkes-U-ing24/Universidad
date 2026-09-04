6adcf408b93fc5c30ff9f5399147c89b1830b7f3
Sorkes-U-ing24 <al07322002@tecmilenio.mx>
2026-09-04 14:22:15 -0600

    Add files via upload

---
+++ S1_Programacion/A4_pitagroas.py
@@ -0,0 +1,46 @@
+# Cesar Gonzalez Ochoa
+# AL07322002
+# 04/09/2026
+# Tabla pitagorica
+
+# El rpograma se encarga de mostrar primeramente una tabla pitagorica atraves de una funcion en la que la desarolla a traves de una matriz
+# sin que esta este definida previamente, para despues con una segunda funcion con return mostrar el valor de la tabla pitagorica buscando 
+# en la  primera funcion el valor correspondiente sin usar operadores de (*)
+
+#Creacion de tabla
+
+tabla=[]
+for i in range(1,11):
+    fila=[]
+    for j in range(1,11):
+        fila.append(i*j)
+    tabla.append(fila)
+
+#Primera funcio
+def imprimir_tabla(lgk):
+    print("\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t")
+    print("_"*83)
+    for i in range(len(lgk)):
+        print(f"{i+1} |\t", end="")
+        for j in range(len(lgk[i])):
+            print(f"{lgk[i][j]}", end="\t")
+        print("")           
+imprimir_tabla(tabla)
+   
+#Pido datos
+
+dato=int(input("ingresa un numero "))
+dato1=int(input("ingresa el otro numero "))
+
+
+#Segunda funcion con return
+def proceso_impresion(proceso, renglon, columna):
+    chocolate=(proceso[renglon-1][columna-1])
+    
+    return chocolate
+    
+
+chocolate=proceso_impresion(tabla, dato, dato1)
+print(f"{chocolate}")
+    
+   
