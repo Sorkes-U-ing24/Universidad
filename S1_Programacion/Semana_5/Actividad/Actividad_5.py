1f1ea0de9ac1710e3699ed7ba89f6963a71d1f1e
Sorkes-U-ing24 <al07322002@tecmilenio.mx>
2026-09-11 17:34:47 -0600

    Update Actividad_5.py
    Segunda funcion del proceso de diccionario

--- S1_Programacion/Semana_5/Actividad/Actividad_5.py
+++ S1_Programacion/Semana_5/Actividad/Actividad_5.py
@@ -29,5 +29,29 @@ def tuplas():
 
     print(f"suma de la tupla es: {jojolion}")
 
+#Diccionario
+def diccionario():
+    
+    contactos={
+        "paulo": "889-653-424-524",
+        "jose": "337-452-781-921",
+        "jorge": "332-951-731-545"
+    }
+
+    nuevo_nombre = input("Nombre del nuevo contacto: ")
+    nuevo_telefono = input("Teléfono del nuevo contacto: ")
+
+    contactos[nuevo_nombre] = nuevo_telefono
+
+    for  clave in contactos:
+         print(clave)
+
+    def buscar_telefono(diccionario, contacto):
+        return diccionario.get(contacto, "Contacto no encontrado ")
+
+    nombre_a_buscar = input("Nombre a buscar: ")
+    telefono_encontrado = buscar_telefono(contactos, nombre_a_buscar)
+    print(f"Teléfono de {nombre_a_buscar}: {telefono_encontrado}")
+
 
     
